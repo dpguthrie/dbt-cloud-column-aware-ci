62d63ba2a580c60779c186f202af70395b44041f
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-12-31 16:25:37 -0700

    More robust column searching

--- src/node.py
+++ src/node.py
@@ -2,12 +2,13 @@
 import json
 import logging
 import subprocess
+import typing as t
 from dataclasses import dataclass
 
 # third party
-from sqlglot import diff, parse_one
+from sqlglot import diff, exp, parse_one
 from sqlglot.diff import Insert, Move, Remove, Update
-from sqlglot.expressions import Column
+from sqlglot.optimizer.scope import Scope, build_scope, find_all_in_scope
 
 # first party
 from src.config import Config
@@ -18,6 +19,103 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+Edit = t.Union[Insert, Move, Remove, Update]
+
+"""
+
+Take the following scenario (where filter changes):
+In [13]: src = 'select 1 as col, 2 as col_2 from x where col > 5'
+
+In [14]: tgt = 'select 1 as col, 2 as col_2 from x where col > 7'
+
+This will create an Insert and Remove breaking change.  Neither will contain a column
+name, but this could be a breaking change and would need to be checked for each downstream
+model, which wouldn't happen right now.
+
+I would need to manually go and add this to the _impacted_unique_ids set by obtaining
+the lineage for the model section syntax of <node.unique_id>+ via the lineage API.
+* This could be done in a single call if I have multiple nodes like these with the syntax:
+
+--select <node.unique_id>+ <other_node.unique_id>+ <other_other_node.unique_id>+
+
+** Another scenario**
+
+- If I have both column level changes and whole node level breaking changes, the entire
+node level breaking changes should supercede any column level, and we should just assume
+that we need to test everything
+
+"""
+
+
+@dataclass
+class BreakingChange:
+    edit: Edit
+
+    def __post_init__(self):
+        try:
+            self._expr = self.edit.expression
+        except AttributeError:
+            self._expr = self.edit.source
+
+    def _in_cte(self, expr: exp.Expression):
+        return expr.find_ancestor(exp.CTE) is not None
+
+    def _find_cte_alias(self, root: Scope, cte: exp.CTE):
+        try:
+            table_alias = [
+                ta
+                for ta in root.find_all(exp.TableAlias)
+                if ta.find_ancestor(exp.Table).name == cte.alias
+            ][0]
+            return table_alias.name
+        except IndexError:
+            return cte.alias
+
+    def _find_parent_column_name(self, expr: t.Union[exp.Column, exp.Alias]):
+        # Get the CTE that contains this expression
+        cte = expr.find_ancestor(exp.CTE)
+
+        # Build scope for the entire query
+        root = build_scope(expr.root())
+
+        # Get the CTE alias and column name from the original expression
+        cte_alias = self._find_cte_alias(root, cte)
+        column_name = expr.output_name
+
+        # Find all columns in the main SELECT that reference this CTE
+        for column in find_all_in_scope(root, exp.Column):
+            # Only look at columns in the main SELECT (not in CTEs)
+            if not self._in_cte(column):
+                # Check if this column references our CTE column
+                if column.table == cte_alias and column.name == column_name:
+                    # Get the final output name (which may be an alias)
+                    parent_alias = column.find_ancestor(exp.Alias)
+                    if parent_alias:
+                        return parent_alias.output_name
+                    return column.output_name
+
+        # If we couldn't find a reference, return the original name
+        return column_name
+
+    @property
+    def column_name(self) -> t.Union[str, None]:
+        expr = self._expr
+        while True:
+            is_column = expr.key in ["alias", "column"]
+            has_ancestor = expr.find_ancestor(exp.Column, exp.Alias) is not None
+            if is_column and not has_ancestor:
+                # if in CTE, need to find where it's used in case it's a column rename
+                if self._in_cte(expr):
+                    return self._find_parent_column_name(expr)
+                else:
+                    return expr.output_name
+
+            elif expr.depth < 1:
+                return None
+
+            expr = expr.parent
+
+
 @dataclass
 class Node:
     unique_id: str
@@ -25,38 +123,48 @@ class Node:
     source_code: str
 
     def __post_init__(self):
+        self._source_exp = parse_one(self.source_code)
+        self._target_exp = parse_one(self.target_code)
         try:
-            self.diff = diff(parse_one(self.source_code), parse_one(self.target_code))
+            self.changes = diff(self._source_exp, self._target_exp, delta_only=True)
         except Exception as e:
             logger.error(
-                f"There was a problem creating a diff for {self.unique_id}.\n"
+                f"There was a problem creating a diff for `{self.unique_id}`.\n"
                 f"Error: {e}\n\nSource: {self.source_code}\nTarget: {self.target_code}"
             )
-            self.diff = []
+            self.changes = []
 
-    @property
-    def valid_changes(self):
-        def is_valid_change(change: Insert | Move | Remove | Update) -> bool:
-            return change.__class__ not in [Move]
+        # All breaking changes from diff
+        self.breaking_changes = self._get_breaking_changes()
 
-        return [change for change in self.diff if is_valid_change(change)]
+        # We should ignore column level changes if there are any node level changes
+        self.ignore_column_changes = any(
+            bc for bc in self.breaking_changes if bc.column_name is None
+        )
 
-    @property
-    def column_changes(self) -> set[str]:
-        """Returns a set containing column names for all changed columns."""
-        changed_columns = set()
-        for change in self.valid_changes:
-            if hasattr(change, "expression"):
-                expression = change.expression
-                while True:
-                    column = expression.find(Column)
-                    if column is not None:
-                        changed_columns.add(column.name)
-                        break
-                    elif expression.depth < 1:
-                        break
-                    expression = expression.parent
-        return changed_columns
+        if self.ignore_column_changes:
+            self.column_changes = set()
+        else:
+            self.column_changes = {
+                bc.column_name for bc in self.breaking_changes if bc.column_name
+            }
+
+    def _get_breaking_changes(self) -> list[BreakingChange]:
+        breaking_changes = []
+
+        inserts = {e.expression for e in self.changes if isinstance(e, Insert)}
+
+        for edit in self.changes:
+            if not isinstance(edit, Insert):
+                breaking_changes.append(BreakingChange(edit))
+
+            elif isinstance(edit.expression, exp.UDTF) or (
+                not isinstance(edit.expression.parent, exp.Select)
+                and edit.expression.parent not in inserts
+            ):
+                breaking_changes.append(BreakingChange(edit))
+
+        return breaking_changes
 
 
 class NodeManager:
@@ -86,7 +194,13 @@ class NodeManager:
         }
 
     def _get_target_code(self) -> dict[str, dict[str, str]]:
-        cmd = ["dbt", "compile", "-s", "state:modified,resource_type:model"]
+        cmd = [
+            "dbt",
+            "compile",
+            "-s",
+            "state:modified,resource_type:model",
+            "--favor-state",
+        ]
 
         logger.info("Compiling code for any modified nodes...")
 
@@ -170,8 +284,10 @@ class NodeManager:
 
         return modified_nodes
 
-    def _get_impacted_unique_ids_for_node(self, node: Node) -> set[str]:
+    def _get_impacted_unique_ids_for_node_columns(self, node: Node) -> set[str]:
         impacted_unique_ids = set()
+
+        # Column level changes
         for column_name in node.column_changes:
             node_column = f"{node.unique_id}.{column_name}"
             if node_column not in self._node_column_set:
@@ -213,6 +329,33 @@ class NodeManager:
                     self._all_unique_ids.add(data["unique_id"])
             except ValueError:
                 continue
+
+    def _get_impacted_unique_ids_for_nodes(self, nodes: list[Node]) -> set[str]:
+        query = """
+        query Environment($environmentId: BigInt!, $filter: LineageFilter!) {
+            environment(id: $environmentId) {
+                applied {
+            lineage(filter: $filter) {
+                uniqueId
+            }
+            }
+        }
+        """
+        names = [node.unique_id.split(".")[-1] for node in nodes]
+        names_str = "+ ".join(names) + "+"
+        variables = {
+            "environmentId": self.config.dbt_cloud_environment_id,
+            "filter": {
+                "select": f"--select {names_str}",
+                "exclude": f"--exclude {' '.join(names)}",
+                # Only getting models for now
+                "types": ["Model"],
+            },
+        }
+        results = self.config.dbtc_client.metadata.query(query, variables)
+        return {
+            r["uniqueId"] for r in results["data"]["environment"]["applied"]["lineage"]
+        }
 
     def _get_downstream_nodes_from_column(
         self, unique_id: str, column_name: str
@@ -259,11 +402,19 @@ class NodeManager:
         if not self._all_unique_ids:
             return list()
 
+        # Column level changes
         for node in self.nodes:
-            if node.valid_changes:
+            if node.column_changes:
                 self._all_impacted_unique_ids.update(
-                    self._get_impacted_unique_ids_for_node(node)
+                    self._get_impacted_unique_ids_for_node_column(node)
                 )
+
+        # Node level changes
+        nodes = [node for node in self.nodes if not node.ignore_column_changes]
+        if nodes:
+            self._all_impacted_unique_ids.update(
+                self._get_impacted_unique_ids_for_nodes(nodes)
+            )
 
         excluded_nodes = self._all_unique_ids - self._all_impacted_unique_ids
         return [em.split(".")[-1] for em in excluded_nodes]
