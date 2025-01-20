d8cf8732f426ac899d86796a59d0afe93a111509
dpguthrie <douglas.p.guthrie@gmail.com>
2025-01-20 12:49:01 -0700

    add property to protocol

--- src/interfaces/lineage.py
+++ src/interfaces/lineage.py
@@ -2,9 +2,11 @@ from typing import TYPE_CHECKING, Dict, List, Protocol, Set
 
 if TYPE_CHECKING:  # pragma: no cover
     from src.models.node import Node
-
+    from src.config import Config
 
 class LineageServiceProtocol(Protocol):
+    config: "Config"
+
     def get_node_lineage(self, nodes: List["Node"]) -> Set[str]: ...
 
     def get_column_lineage(self, node_id: str, column_name: str) -> Set[str]: ...
